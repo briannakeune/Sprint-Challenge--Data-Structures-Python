5936230a30684fa87162a3ab8767e6471db5c6c7
Brianna <briannakeune@gmail.com>
2020-05-15 10:34:06 -0500

    reverse is passing

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -38,5 +39,29 @@ class LinkedList:
 
         return False
 
+    # bug I ran into here was figuring out how to stop the recursion,
+    # used brute force to get that portion
+    # I need to practice asking more questions, and trying to understand
+    # the whole picture of the problem first
     def reverse_list(self, node, prev):
-        pass
+        # must use recursion
+        # and the recursion stopper is...when the node is none
+        if node is None:
+            self.head = prev
+            return
+        # if linked list is:
+        # 1 -> 2 -> 3 -> 4
+        # output should be:
+        # 4 -> 3 -> 2 -> 1
+        swapping_node = node.next_node  # if node is 1, then this is 2
+        node.next_node = prev  # if node is 1, then 1's next node is None
+        prev = node  # if node is 1, new prev arg will be 1
+        node = swapping_node  # new node arg will be 2
+        # output round 1 would be
+        # 1 -> None
+        # and then redoing the process of
+        # placing the node's prev value as it's new next value
+        self.reverse_list(node, prev)
+        # round two args (2, 1)
+        # round two output
+        # 2 -> 1 -> None
