4e0e63ecdf090d5225c48664ab81dcaf4a576622
Brianna <briannakeune@gmail.com>
2020-05-15 07:36:42 -0500

    ring_buffer passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
+# a queue with a size limit
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.count = 0
+
+    '''
+    Adds an item to the ring buffer in order,
+    if max capacity is reached,
+    the item replaces the oldest object
+    '''
+    # O(n) ?
 
     def append(self, item):
-        pass
+        # check current item list
+        # if it is lower
+        if len(self.storage) < self.capacity:
+            # add the item to our queue/list
+            self.storage.append(item)
+        # else if it is greater
+        else:
+            # reseting where to place next index counter
+            if self.count == self.capacity:
+                self.count = 0
+            # swap out item in the index that matches count
+            # with new item
+            self.storage[self.count] = item
+            self.count += 1
+
+        # stretch:
+        # update this into a linked list
 
     def get(self):
-        pass
+        return self.storage
